c4b68d7629b9cdd217cd74abd3d89cb0577de60a
Lily <836876398@qq.com>
2017-11-21 23:14:25 +0800

    exports

--- module/class.js
+++ module/class.js
@@ -1,4 +1,4 @@
-var Teacher = require('./teacher');
+var Teacher = require('./teacher');     //  require(xxx)de 返回值：module.exports
 var Student = require('./student');
 
 function add (teacher, students) {
@@ -10,3 +10,7 @@ function add (teacher, students) {
 
 exports.add = add;      //  想让模块成为传统的模块实例
 //module.exports = add;   //  想让模块成为一个特别的对象类型
+
+//console.log(module)
+//console.log(exports === module.exports);    //  true
+//如果给module.exports 或 exports重新赋值， require得到的将是修改的值，这点跟js对象的对象不同
